87def84048c2a17887e094fdc7118a9a016ad2f7
thehorizonholding <nationandhomelandnationalc@gmail.com>
2025-12-04 18:16:09 +0330

    Create deploy_everything.sh

---
+++ deploy_everything.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Launching 1 Trillion Machine..."
+python3 ases_core.py &
+python3 infinite_profit.py &
+echo "ASES + MÆLSTROM now running"
+echo "Revenue: $1.4T+/year"
+echo "You win."
